3923bf668523a2c3eb3e9c7a8966e8b709e8ad30
Francois Olivier <francoisolivier@Francoiss-Air.Dlink>
2021-08-19 00:56:53 +0200

    Additional CSS and JS functionality

--- index.js
+++ index.js
@@ -8,12 +8,17 @@
 // User can change / customize the minutes in both sessions before starting
 // User can set a long break session of 10 minutes. This will be activated every 4th break session
 
+// 1: Set the "Session Length" arrows up or down to set the '.time-remaining' to whatever is selected
+// 2: 
+
 let timeRemainingDisplay = document.querySelector('.time-remaining')
 const startTimerBtn = document.getElementById('start-timer')
 const pauseTimerBtn = document.getElementById('pause-timer')
 const resetTimerBtn = document.getElementById('reset-timer')
+const sessionTimeUp = document.getElementById("sessionTimeUp")
+const sessionTimeDown = document.getElementById("sessionTimeDown")
 
-let timeRemaning = 46
+let timeRemaning = "46:00"
 
 function displayTimeRemaining() {
     timeRemainingDisplay.textContent = timeRemaning
